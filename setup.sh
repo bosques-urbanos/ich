ed21c8bed55b26ee4843201293bdb31fd97cf832
Abraham Arce <xe1gyq@gmail.com>
2020-02-22 13:39:14 -0600

    Ich: Setup
    Welcome initial setup script! Not good but enough for now.
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# General Packages
+sudo apt-get update
+sudo apt-get install influxdb-client mosquitto-clients ffmpeg
+
+# Docker
+# Tbd
+
+# Docker Compose
+# Tbd
+
+# Ich Gitlab Repository
+cd ~/ich/
+git clone https://github.com/bacabhome/www.git homeassistant/www
+git clone https://github.com/bacabhome/plex.git homeassistant/www/plex/
+git clone https://gitlab.com/bosques-urbanos/iikim/iikim.git
+docker-compose pull
+docker-compose build
+
+# Services
+cd ~/ich/services
+docker-compose build
+
+# Iikim Applications
+cd ~/ich/iikim
+docker-compose build
+
+# Intel OpenVINO Toolkit
+cd ~/ich/iikim/openvino
+make demo-cpu
+
+# InfluxDB Database
+
+cd ~/ich
+docker-compose up -d influxdb
+sleep 5
+influx -execute 'CREATE DATABASE home_assistant'
+
+# Home Assistant Secrets File Configuration
+cp config/homeassistant/secrets.yaml homeassistant/
+
+# Ich
+docker-compose up -d
